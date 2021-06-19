1cea9c16a38e828be267e73354ca4452525a5384
Initial P <panji2setiawan@icloud.com>
2021-06-19 20:34:09 +0700

    error handling duplicate key error

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -21,10 +21,16 @@ module.exports = (err, req, res, next) => {
             const message = `Resource not found. Invalid ${err.path}`
             error = new ErrorHandler(message,400)
         }
-        
+
         // Handling Mongoose Validation Error
         if(err.name === "ValidationError"){
             const message = Object.values(err.errors).map(values => values.message);
+            error = new ErrorHandler(message,400)
+        }
+
+        // Hadling Mongoose duplicate key error
+        if(err.code === 11000){
+            const message = `Duplicate ${Object.keys(err.keyValue)} entered`
             error = new ErrorHandler(message,400)
         }
 
